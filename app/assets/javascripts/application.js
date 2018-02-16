80e1bc5a9476349f58db881aab58e8ea2a4f8125
Nayab <nbsamar@gmail.com>
2018-02-16 14:54:23 +0530

    setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,4 +11,14 @@
 // about supported directives.
 //
 //= require rails-ujs
+import "bootstrap-sprockets";
+import "bootstrap";
+import "font-awesome";
+import "jquery.datetimepicker";
+import "*";
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require bindWithDelay
+//= require jquery.datetimepicker
 //= require_tree .
---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,5 @@
+class HomepageController < ApplicationController
+  def nav
+
+  end
+end
